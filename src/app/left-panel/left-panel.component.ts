a5b24403ea3b41658f107ef31efaa21d1ec9fe16
Mr. Undefined <service.b@blog.guidefather.in>
2020-07-02 23:22:32 +0530

    pages introduced

--- src/app/left-panel/left-panel.component.ts
+++ src/app/left-panel/left-panel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-left-panel',
@@ -6,15 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-panel.component.css']
 })
 export class LeftPanelComponent implements OnInit {
-  viewType:string="Para";
-  MaxSent:number=5;
-  MinSent:number=10;
-  MaxWord:number=10;
-  MinWord:number=15;
-  Words:string="";
-  constructor() { }
+  viewType: string = "Para";
+  MaxSent: number = 5;
+  MinSent: number = 10;
+  MaxWord: number = 10;
+  MinWord: number = 15;
+  Words: string = "";
 
-  ngOnInit(): void {
+  constructor(private router: Router, private route: ActivatedRoute) {
   }
 
+  ngOnInit(): void {
+
+    let queryParams = this.route.snapshot.queryParams;
+    if (queryParams.viewType) this.viewType = queryParams.viewType;
+    if (queryParams.MaxSent) this.MaxSent = queryParams.MaxSent;
+    if (queryParams.MinSent) this.MinSent = queryParams.MinSent;
+    if (queryParams.MaxWord) this.MaxWord = queryParams.MaxWord;
+    if (queryParams.MinWord) this.MinWord = queryParams.MinWord;
+    if (queryParams.Words) this.Words = queryParams.Words;
+  }
+
+  updateRoute() {
+    let obj = {};
+    if (this.viewType = "Para") {
+      obj["viewType"] = "Para",
+        obj["MaxSent"] = "MaxSent",
+        obj["MinSent"] = "MinSent"
+    }
+    const urlTree = this.router.createUrlTree([], {
+      queryParams: obj,
+      queryParamsHandling: "merge",
+      preserveFragment: true
+    });
+    this.router.navigateByUrl(urlTree);
+  }
 }
